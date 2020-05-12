b34cce2620989706af9e08ff589ddd171f88d42e
Neet Mangat <neetmangat@gmail.com>
2020-05-12 08:37:56 +0000

    Fixed Links and Form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,8 @@ class QuotesController < ApplicationController
     def index
         @quote = Quote.order(Arel.sql("RANDOM()")).first
     end
+
+    def new
+        @quote = Quote.new
+    end
 end
