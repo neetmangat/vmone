fd437ba88e1d62a7a03b2b1c90c3f5184a94dadb
neetmangat <neetmangat@gmail.com>
2020-05-27 05:33:56 +0000

    Design Retouch

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,2 @@
 class Quote < ApplicationRecord
-    validates :saying, presence: true, length: { maximum: 300, minimum: 5 };
-    validates :author, presence: true, length: { maximum: 9, minimum: 4 }
 end
