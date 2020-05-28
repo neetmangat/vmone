b81014f7437cc82caf2bef386ad1a26633f5d5bd
neetmangat <neetmangat@gmail.com>
2020-05-28 06:16:11 +0000

    Validations added

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+    validates :saying, presence: true , length: { maximum: 200, minimum: 3 }
+    validates :author, presence: true , length: { maximum: 20, minimum: 3 }
 end
